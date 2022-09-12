58ed30b4eca3f6d56cc4998a185d8471a6b4b0e8
ezequiel villegas <ezevillegas0@gmail.com>
2022-09-12 10:23:31 -0300

    trabajo en la clase 16

--- proyecto/SRC/clase 1 - 16/clase16.js
+++ proyecto/SRC/clase 1 - 16/clase16.js
@@ -38,6 +38,22 @@
 
 //ENTREGA DEL PROYECTO FINAL
 
+/* 
+Objetivos específicos:
+-Contar con una estructura de datos clara, basada en Arrays y Objetos.
+-Utilizar funciones, condicionales e iteradores para manipular los datos de la app.
+-Generar y manipular el DOM. Crear vistas a partir de datos de la app y generar eventos para responder a la interacción del usuario. Utilizar alguna -librería relevante para el simulador.
+-Utilizar asincronía y fetch para cargar datos estáticos o consumir una API.
 
+El proyecto final debe incluir:
+-Objetos y Arrays. Métodos de Arrays.
+-Funciones y condicionales.
+-Generación del DOM de forma dinámica. Eventos.
+-Sintaxis avanzada.
+-Al menos una librería de uso relevante para el proyecto.
+-Manejo de promesas con fetch. 
+-Carga de datos desde un JSON local o desde una API externa.
+
+*/
 
 /* ---------------------------------------------------------------------- */
