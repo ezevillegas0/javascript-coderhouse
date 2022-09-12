3f3e18a8eb0aa564b2a2b5e8708a35c6be8fba00
ezequiel villegas <ezevillegas0@gmail.com>
2022-09-12 10:19:44 -0300

    trabajo en la clase 16

--- proyecto/SRC/clase 1 - 16/clase16.js
+++ proyecto/SRC/clase 1 - 16/clase16.js
@@ -2,55 +2,42 @@
 
 //
 
+                           //CLASE 16
+
+                    //FRAMEWORKS & NODE JS
+
+//NODE JS
+
+//Es un entorno adicional de ejecución para Javascript. Es un programa que permite ejecutar código JavaScript por fuera del navegador. Con él podemos construir desde aplicaciones de línea de comandos (consola) hasta servidores HTTP para potenciar aplicaciones web.
+
+/* ---------------------------------------------------------------------- */
+
+//Backend con NodeJS
+
+//Al trabajar en el back no disponemos de la UI del navegador para ir testeando los resultados. La principal herramienta para esto es la consola cli (command line interface). 
+//Con ella podemos ejecutar nuestro programa en el servidor, ver los logs que generamos, visualizar errores y mensajes informativos explícitamente generados por Javascript en tiempo de ejecución dentro del contexto del programa del servidor 🤩.
+
+/* ---------------------------------------------------------------------- */
+
+//NPM
+
+//Es un repositorio de módulos de Node. Muchos de esos programas que incorporamos a nuestros proyectos están escritos como paquetes, o módulos, de Node. En el repositorio NPM se listan y alojan para descargarlos e integrarlos a las APPs de forma sencilla 🙌
+/* 
+-el comando NPM nos permite acceder a este repositorio y administrar estos paquetes
+-el comando NPM install nos permite instalar cualquier modulo disponible en este repositorio.
+*/
+
+/* ---------------------------------------------------------------------- */
+
+//SPA
+
+//Una SPA (Simple Page Application), o aplicación de una sola página, es una web app que consiste de un único archivo html que se carga al ingresar al sitio, y el resto de la navegación y cambios de página se da mediante modificación dinámica del DOM con Javascript.
+//Una SPA tiene el propósito de brindar una experiencia más fluida al usuario, mejorando la performance y velocidad 👤. ¿De qué manera hace esto? 🙀 En ella, todos los códigos de HTML, JavaScript, y CSS se cargan una sola vez​ o los recursos necesarios se cargan dinámicamente cuando lo requiera la página, normalmente como respuesta a las acciones del usuario.
+
+/* ---------------------------------------------------------------------- */
+
+//ENTREGA DEL PROYECTO FINAL
 
 
 
 /* ---------------------------------------------------------------------- */
-
-/* ---------------------------------------------------------------------- */
-
-/* ---------------------------------------------------------------------- */
-
-/* ---------------------------------------------------------------------- */
-
-/* ---------------------------------------------------------------------- */
-
-/* ---------------------------------------------------------------------- */
-
-/* ---------------------------------------------------------------------- */
-
-/* ---------------------------------------------------------------------- */
-
-/* ---------------------------------------------------------------------- */
-
-/* ---------------------------------------------------------------------- */
-
-/* ---------------------------------------------------------------------- */
-
-/* ---------------------------------------------------------------------- */
-
-/* ---------------------------------------------------------------------- */
-
-/* ---------------------------------------------------------------------- */
-
-/* ---------------------------------------------------------------------- */
-
-/* ---------------------------------------------------------------------- */
-
-/* ---------------------------------------------------------------------- */
-
-/* ---------------------------------------------------------------------- */
-
-
-/* ---------------------------------------------------------------------- */
-
-/* ---------------------------------------------------------------------- */
-
-/* ---------------------------------------------------------------------- */
-
-/* ---------------------------------------------------------------------- */
-
-/* ---------------------------------------------------------------------- */
-
-/* ---------------------------------------------------------------------- */
-
